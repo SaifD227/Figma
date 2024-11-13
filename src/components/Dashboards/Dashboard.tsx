5e55731725a015a2890748535ea8ae0548a7fdb7
SaifD227 <saifaalii237@gmail.com>
2024-11-13 16:19:09 +0500

    dashboard import removed

--- src/components/Dashboards/Dashboard.tsx
+++ src/components/Dashboards/Dashboard.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import MyCards from "./MyCards";
-// import WeeklyActivity from "./WeeklyActivity"
+
 
 const Dashboard = () => {
   return (
     <div>
       <MyCards />
-      {/* <WeeklyActivity/> */}
+  
     </div>
   );
 };
